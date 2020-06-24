9c2c7f00b512f72295f92e8eb628f442a8e8c797
Laura <laura.robertson64@gmail.com>
2020-06-23 23:22:41 -0500

    set user agent header for GET polling address route

--- server/fetchPollingLocations.js
+++ server/fetchPollingLocations.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
-
+const {randomUserAgent} = require('./utils/randomUserAgent');
 const {grabAllAddresses} = require('./states/texas');
 
 
@@ -12,7 +12,13 @@ router.use(bodyParser.json());
 
 router.post('/', async (req, res) => {
   const {idElection, vuid} = req.body;
-  const electionResp = await axios.get(`https://teamrv-mvp.sos.texas.gov/MVP/getPollPlaces.do?idElection=${idElection}&idVoter=${vuid}`);
+  const headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'user-agent': randomUserAgent()
+  }
+  const electionResp = await axios({method: 'GET', url:`https://teamrv-mvp.sos.texas.gov/MVP/getPollPlaces.do?idElection=${idElection}&idVoter=${vuid}`, headers});
+  // const electionResp = await axios.get(`https://teamrv-mvp.sos.texas.gov/MVP/getPollPlaces.do?idElection=${idElection}&idVoter=${vuid}`);
   const pollingAddresses = await grabAllAddresses(electionResp.data);
 
   res.send(pollingAddresses);
